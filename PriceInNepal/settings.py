8a09db17bbc440fe9cd85c973e6a807dd9101f60
amin <amin.is.don.bujis@gmail.com>
2024-03-07 13:11:58 +0545

    init static files

--- PriceInNepal/settings.py
+++ PriceInNepal/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
